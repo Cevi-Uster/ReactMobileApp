7bffdce77038136846e21221508c85cb88e99ab2
Marc Baumgartner <mbaumgar@MarcBauMacBook2.fritz.box>
2018-04-05 22:27:34 +0200

    Load root event categories from tribe calendar from new Cevi Uster webpage for agenda.

--- CeviUster/Screens/AgendaScreen.js
+++ CeviUster/Screens/AgendaScreen.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
 
 export default class AgendaScreen extends React.Component {
+
+  state = {
+    data: [],
+    currentParentId: 0,
+  };
+
+  componentWillMount(){
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const response = await fetch("https://new.cevi-uster.ch/wp-json/tribe/events/v1/categories/?hide_empty=false&orderby=parent");
+    const json = await response.json();
+    const filteredCategories = json.categories.filter(categorie => categorie.parent == this.state.currentParentId);
+    this.setState({data: filteredCategories});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-
+        <FlatList
+          data={this.state.data}
+          keyExtractor={(x, i) => i}
+          renderItem={({item}) => <Text>{item.name}</Text>}>
+          </FlatList>
       </View>
     );
   }
