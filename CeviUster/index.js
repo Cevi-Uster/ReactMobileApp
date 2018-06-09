ba313548d3ccec1d76bc311ce2277259155b5dd8
Marc Baumgartner <mbaumgar@mb01.fritz.box>
2018-06-09 21:52:34 +0200

    Ignore warnings.

--- CeviUster/index.js
+++ CeviUster/index.js
@@ -1,3 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+console.ignoredYellowBox = ['Remote debugger'];
 AppRegistry.registerComponent('CeviUster', () => App);
