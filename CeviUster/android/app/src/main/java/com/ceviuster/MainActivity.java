f7e5c968bdd41ff8df28d4b88438aa09583c3ae8
Marc Baumgartner <marc@mabaka.ch>
2019-02-19 21:47:36 +0100

    Access to system calenders on Android fixed

--- CeviUster/android/app/src/main/java/com/ceviuster/MainActivity.java
+++ CeviUster/android/app/src/main/java/com/ceviuster/MainActivity.java
@@ -1,6 +1,12 @@
 package com.ceviuster;
 
+import com.calendarevents.CalendarEventsPackage;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +17,18 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "CeviUster";
+    }
+
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new CalendarEventsPackage()
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        CalendarEventsPackage.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
