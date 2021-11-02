4d07d33e5997fbd1f363cd4e1c5992a7534c03a1
Marc Baumgartner <marc@mabaka.ch>
2021-11-02 21:25:43 +0100

    Upgrade to RN 0.61.5 and fix iOS version

--- CeviUster/App.js
+++ CeviUster/App.js
@@ -112,7 +112,10 @@ export default function App() {
             let iconName = 'warning-outline';
             if (route.name === 'Welcome') {
               //iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-              return <Image source={require('./Ressources/Home_Icon.png')} style={[{resizeMode: 'contain'}, {tintColor: color}]} />;
+              return <Image 
+                source={require('./Ressources/Home_Icon.png')} 
+                style={[{resizeMode: 'contain'}, {tintColor: color}]} 
+                />;
             }  else if (route.name === 'Agenda') {
               iconName = `ios-calendar${focused ? '' : '-outline'}`;
             } else if (route.name === 'Infobox') {
@@ -139,49 +142,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-/*export default TabNavigator(
-  {
-    Willkommen: { screen: WelcomeStack, navigationOptions: { tabBarLabel: 'Willkommen' } },
-    Agenda: { screen: AgendaStack, navigationOptions: { tabBarLabel: 'Agenda' } },
-    InfoBox: { screen: InfoBoxStack, navigationOptions: { tabBarLabel: 'Chäschtli' } },
-    Kontakt: { screen: ContactStack, navigationOptions: { tabBarLabel: 'Kontakt' } },
-    Datenschutz: { screen: PrivacyStatementStack, navigationOptions: { tabBarLabel: 'Datenschutz' } },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        var styles = StyleSheet.create({
-        PNGImageStyle: {
-          width: 25,
-          height: 25
-        }});
-        if (routeName === 'Willkommen') {
-          //iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-          return <Image source={require('./Ressources/Home_Icon.png')} style={[styles.PNGImageStyle, {tintColor: tintColor}]} />;
-        } else if (routeName === 'Kontakt') {
-          iconName = `ios-contact${focused ? '' : '-outline'}`;
-        } else if (routeName === 'Agenda') {
-          iconName = `ios-calendar${focused ? '' : '-outline'}`;
-        } else if (routeName === 'InfoBox') {
-          iconName = `ios-bonfire${focused ? '' : '-outline'}`;
-        } else if (routeName === 'Datenschutz') {
-          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-        }
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: 'navy',
-      inactiveTintColor: 'gray',
-    },
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: false,
-  }
-);*/
